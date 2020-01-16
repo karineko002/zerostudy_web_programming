ab96251e712133238b347e5ca50ab74cb8839932
karineko002 <hamagawa.m@gmail.com>
2020-01-16 22:58:19 +0900

    直線と多角形を描く

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 function draw(){
+  // ▽
+	var pdata1 = [
+		[50,100],
+		[350,100],
+		[200,300]
+  ];
+  // △
+	var pdata2 = [
+		[50,250],
+		[350,250],
+		[200,50]
+	];
 	var canvas = document.querySelector('#canvas');
 	var context = canvas.getContext('2d');
+	context.strokeStyle = "black";
 	context.beginPath();
-	context.fillStyle = "rgba(255,0,0,0.5)";
-	for (var i = 0; i < 10; i++){
-    // 開始
-    context.beginPath();
-    // パス
-		context.arc(100 + 25 * i, 100 + 25 * i, 50, 0, Math.PI * 2, false);
-    // 描画
-    context.fill();
-  }
-  // 最後の描画
+	var p = pdata1[0];
+  context.moveTo(p[0], p[1]);
+  // ▽
+	for (var i = 1; i < pdata1.length; i++){
+		p = pdata1[i];
+		context.lineTo(p[0], p[1]);
+	}
+	context.closePath();
+	context.fillStyle = "rgba(0,255,0,0.5)";
 	context.fill();
+	context.stroke();
+	context.beginPath();
+	var p = pdata2[0];
+  context.moveTo(p[0], p[1]);
+  // △
+	for (var i = 1; i < pdata2.length; i++){
+		p = pdata2[i];
+		context.lineTo(p[0], p[1]);
+	}
+	context.closePath();
+	context.fillStyle = "rgba(0,0,255,0.5)";
+	context.fill();
+	context.stroke();
 }
-
