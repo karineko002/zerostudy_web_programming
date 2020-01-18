9616fc1a1c07901306cf2939c34bf87df0aa6408
karineko002 <hamagawa.m@gmail.com>
2020-01-18 18:43:50 +0900

    グラフィックを描く

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function draw(){
-	var pdata = [
-		[50,100],
-		[100,300],
-		[250,-100],
-		[300,100]
-	];
 	var canvas = document.querySelector('#canvas');
 	var context = canvas.getContext('2d');
-	context.fillStyle = "#FF9999";
-	context.strokeStyle = "blue";
-	context.beginPath();
-	context.moveTo(pdata[0][0], pdata[0][1]);
-	context.bezierCurveTo(
-		pdata[1][0],pdata[1][1],pdata[2][0],
-		pdata[2][1],pdata[3][0],pdata[3][1]);
-	context.stroke();
-
+	context.fillStyle = 'rgb(255,200,200)';
+	context.fillRect(0, 0, 500, 400);
+	
+	var img = new Image();
+	img.src = "character.png";
+	img.onload =function(){
+	context.drawImage(img,50,50);
+}
 }
