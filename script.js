29fd3ca46978a009b274f661d90aa12d6a961958
karineko002 <hamagawa.m@gmail.com>
2020-01-20 12:42:39 +0900

    リスト4-12　キーボードで操作する

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// グローバル関数
 var canvas;
 var img, bkimg;
+var x = 200;
+var y = 200;
 
 //初期化の処理
 function initial(){
-		canvas = document.querySelector('#canvas');
-		canvas.onclick =draw;
-		
-		img = new Image();
-		img.src = "character.png";
-		bkimg = new Image();
-		bkimg.src = "background.png";
-		bkimg.onload = function(){
-			drawBackgtound();
-		}
+	canvas = document.querySelector('#canvas');
+	canvas.onkeydown = draw; 
+	
+	img = new Image();
+	img.src = "character.png";
+	bkimg = new Image();
+	bkimg.src = "background.png";
+	bkimg.onload = function(){
+		drawBackground();
+	}
 }
 
 // クリックしたときの処理
@@ -24,14 +27,26 @@ function draw(event){
 // 背景の描画
 function drawBackground(){
 	var context = canvas.getContext('2d');
-	context.clearRect(0,0,500,400);
-	context.drawImage(bkimg,0,0,500,400);
+	context.clearRect(0, 0, 500, 400);
+	context.drawImage(bkimg, 0, 0, 500, 400);
 }
 
 // イメージの描画
 function drawImage(event){
 	var context = canvas.getContext('2d');
-	var x = event.clientX - canvas.offsetLeft;
-	var y = event.clientY - canvas.offsetTop;
-	context.drawImage(img,x - img.width / 2,y - img.height / 2);
+	switch(event.code){
+		case 'ArrowLeft':
+		x -= 10;
+		break;
+		case 'ArrowRight':
+		x += 10;
+		break;
+		case 'ArrowUp':
+		y -= 10;
+		break;
+		case 'ArrowDown':
+		y += 10;
+		break;
+	}
+	context.drawImage(img,x, y);
 }
