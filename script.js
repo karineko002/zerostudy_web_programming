e4067cf6ddeb8480b5525a2cf93b128518fe17f5
karineko002 <hamagawa.m@gmail.com>
2020-01-20 13:49:15 +0900

    イメージをアニメーションさせる

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // グローバル関数
 var canvas;
 var img, bkimg;
-var x = 200;
-var y = 200;
+var x = 0;
+var timer;
 
 //初期化の処理
 function initial(){
 	canvas = document.querySelector('#canvas');
-	canvas.onkeydown = draw; 
+	// canvas.onkeydown = draw; 
 	
 	img = new Image();
 	img.src = "character.png";
 	bkimg = new Image();
 	bkimg.src = "background.png";
 	bkimg.onload = function(){
-		drawBackground();
+	timer = setInterval(draw, 50);
 	}
 }
 
 // クリックしたときの処理
-function draw(event){
+function draw(){
 	drawBackground();
-	drawImage(event);
+	drawImage();
 }
 
 // 背景の描画
@@ -32,21 +32,9 @@ function drawBackground(){
 }
 
 // イメージの描画
-function drawImage(event){
+function drawImage(){
 	var context = canvas.getContext('2d');
-	switch(event.code){
-		case 'ArrowLeft':
-		x -= 10;
-		break;
-		case 'ArrowRight':
-		x += 10;
-		break;
-		case 'ArrowUp':
-		y -= 10;
-		break;
-		case 'ArrowDown':
-		y += 10;
-		break;
-	}
-	context.drawImage(img,x, y);
+	x += 5;
+	context.drawImage(img, x, 100);
+	if (x > 400){ clearInterval(timer); }
 }
