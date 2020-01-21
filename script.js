50e645ff4941f4a399917608cae1b0f2136939a6
karineko002 <hamagawa.m@gmail.com>
2020-01-22 01:52:16 +0900

    キャラクタをオブジェクトにまとめよう

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // グローバル関数
 var canvas;
 var img, bkimg;
-var x = 100;
-var y = 100;
-var dx = 5;
-var dy = 5;
-
+var anim;
+var timer;
 
 //初期化の処理
 function initial(){
@@ -15,32 +12,67 @@ function initial(){
 	bkimg = new Image();
 	bkimg.src = "background.png";
 	bkimg.onload = function(){
-	timer = setInterval(draw, 50);
+		anim = new Anim();
+		canvas.onclick = makeCharacter;
+		timer = setInterval(doTimer, 50);
 	}
 }
 
 // クリックしたときの処理
-function draw(){
-	drawBackground();
-	drawImage();
+function makeCharacter(event){
+	anim.click(event);
 }
 
-// 背景の描画
-function drawBackground(){
-	var context = canvas.getContext('2d');
-	context.clearRect(0, 0, 500, 400);
-	context.drawImage(bkimg, 0, 0, 500, 400);
+// タイマーで実行する処理
+function doTimer(){
+	anim.draw();
 }
 
-// イメージの描画
-function drawImage(){
-	var context = canvas.getContext('2d');
-	x += dx;
-	y += dy;
-	if(x < 0){dx *= -1;}
-	if(y < 0){dy *= -1;}
-	if(x + img.width > 500){dx *= -1;}
-	if(y + img.height > 400){dy *= -1;}
-	context.drawImage(img, x, y);
+// アニメーションを管理するオブジェクト
+function Anim(){
+	this.imgs = [];
 
+	// クリックした地点にAnimImageを作成
+	this.click = function(event){
+		var x = event.clientX - canvas.offsetLeft - img.width / 2;
+		var y = event.clientY - canvas.offsetTop - img.height / 2;
+		this.imgs.push(new AnimImage(img, x, y));
+	}
+
+	// イメージの描画
+	this.draw = function(){
+		this.drawBackground();
+		for(var n in this.imgs){
+			this.imgs[n].drawImage();
+		}
+	}
+
+	// 背景の描画
+	this.drawBackground =function(){
+		var context = canvas.getContext('2d');
+		context.clearRect(0, 0, 500, 400);
+		context.drawImage(bkimg, 0, 0, 500, 400);
+	}
 }
+
+// アニメーションキャラクタのオブジェクト
+function AnimImage(img, x, y){
+	this.img = img;
+	this.x = x;
+	this.y = y;
+	this.dx = Math.floor(Math.random() * 10) + 1;
+	this.dy = Math.floor(Math.random() * 10) + 1;
+	// イメージの描画
+	this.drawImage = function(){
+		var context = canvas.getContext('2d');
+		this.x += this.dx;
+		this.y += this.dy;
+		if(this.x < 0){this.dx *= -1;}
+		if(this.y < 0){this.dy *= -1;}
+		if(this.x + img.width > 500){this.dx *= -1;}
+		if(this.y + img.height > 400){this.dy *= -1;}
+		context.drawImage(this.img, this.x, this.y);
+
+	}
+}
+
