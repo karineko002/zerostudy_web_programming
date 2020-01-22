f426e4632639ca9efc8aacb9f9d5a20d49653a4d
karineko002 <hamagawa.m@gmail.com>
2020-01-22 20:17:58 +0900

    <!-- Chaoter5 リスト5-4　foreachという配列用の繰り返しを使う -->

--- Hello.php
+++ Hello.php
@@ -1,4 +1,4 @@
-<!-- Chaoter5 リスト5-2　配列と連想配列 -->
+<!-- Chaoter5 リスト5-4　foreachという配列用の繰り返しを使う -->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -16,10 +16,10 @@
   <h1>Hello!</h1>
   <p>
   <?php 
-    $data = [98,79,56,83,69];
+    $data = [98,79,56,83,70];
     $total = 0;
-    for($i = 0;$i < count($data); $i++){
-      $total += $data[$i];
+    foreach($data as $item){
+      $total += $item;
     }
     echo "データの合計は、 {$total} です。";
 
