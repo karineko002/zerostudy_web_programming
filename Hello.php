70982b08866e6fb7944f932499ce1cc5bc6c5b50
karineko002 <hamagawa.m@gmail.com>
2020-01-22 13:39:29 +0900

    Chaoter5 リスト5-1　サーバープログラムを作ろう

---
+++ Hello.php
@@ -0,0 +1,21 @@
+<!-- Chaoter5 リスト5-1　サーバープログラムを作ろう -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Hello!</title>
+  <style>
+  body{font-size:14pt; font-weight:plain;}
+  h1 {color:white; font-size:24pt;
+    background-color:green;}
+  table td{background-color:#aaffaa;}
+  </style>
+</head>
+
+<body>
+  <h1>Hello!</h1>
+  <p><?php echo "これはPHPのサンプルです。"; ?></p>
+  </body>
+  </html>
+
+  <!-- access http://localhost/zerostudy_web_programming/hello.php -->
