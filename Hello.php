941b3e40a3f47f3dc3316349c7e1d53c8e78d907
karineko002 <hamagawa.m@gmail.com>
2020-01-25 21:13:58 +0900

    ファイルへの書き出し

--- Hello.php
+++ Hello.php
@@ -1,4 +1,4 @@
-<!-- Chaoter5 リスト5-17　一行ごとに取り出す -->
+<!-- Chaoter5 リスト5-18　ファイルへの書き出し -->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,14 +9,30 @@
 	h1 { color:white;font-size:24pt;
 		background-color:red; }
   table td { background-color:#aaffaa; }
-	</style>
+  </style>
+  <?php 
+  // ファイルを開く　fopen
+  if(isset($_POST['msg'])){
+    $msg = $_POST['msg'];
+    $f = @fopen('savedata.txt','a');
+  // ファイルからデータを読み書きする関数を呼び出す　fputs
+    fputs($f,$msg . "\n");
+  // リソースを開放する　fclose
+    fclose($f);
+  }
+  ?>
 </head>
 
 <body>
   <h1>Hello!</h1>
-  <table>
-  <?php 
-  $data = @file('sample.txt') or
+  <form method="post" action="/zerostudy_web_programming/hello.php">
+    <input type="text" name="msg">
+    <input type="submit">
+</for>
+<hr>
+<table>
+  <?php
+  $data = @file('savedata.txt') or
     exit('<tr><td>ファイルの読み込みに失敗しました。</td></tr>');
 
   $num = 0;
@@ -25,7 +41,7 @@
     echo "<tr><td>{$num}</td><td>{$line}</td></tr>";
   }
   ?>
-  </table>
+</table>
 </body>
 </html>
 
